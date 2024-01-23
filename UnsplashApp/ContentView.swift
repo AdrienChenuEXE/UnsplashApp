9b6404e2cff3c2c222f4e6e371b89c60d7ff9485
Adrien Chenu <adrienchenudu73@gmail.com>
2024-01-23 04:51:09 -0800

    Initial Commit

---
+++ UnsplashApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnsplashApp
+//
+//  Created by Adrien CHENU on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
