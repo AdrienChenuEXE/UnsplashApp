744991730831886209dcc9a58abfb555b037cdc8
Adrien Chenu <adrienchenudu73@gmail.com>
2024-01-23 07:11:02 -0800

    gitignore and first grid visu

--- UnsplashApp/ContentView.swift
+++ UnsplashApp/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var imageURLs = [
+        "https://images.unsplash.com/photo-1704146087769-ba4d31543936?ixid=M3w1NTc1NTR8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDYwMTQwODR8\\u0026ixlib=rb-4.0.3",
+        "https://images.unsplash.com/photo-1563473213013-de2a0133c100?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwyfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1490349368154-73de9c9bc37c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwzfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1495379572396-5f27a279ee91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw0fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://cdn.discordapp.com/attachments/955936878077562900/1197820770567602257/IMG_2320.png?ex=65bca886&is=65aa3386&hm=0b2a0b0eee46735bfd2ebe2aa082ae600d8b8a38e8e321872dcfbc8616964624&",
+        "https://images.unsplash.com/photo-1695653422715-991ec3a0db7a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MXwxfGFsbHw2fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1547327132-5d20850c62b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw3fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw4fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1475694867812-f82b8696d610?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw5fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1558816280-dee9521ff364?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwxMHx8fHx8fDF8fDE3MDM3NTk1NTF8&ixlib=rb-4.0.3&q=80&w=1080"
+    ]
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 8) {
+                    ForEach(imageURLs, id: \.self) { imageUrl in
+                        AsyncImage(url: URL(string: imageUrl)) { image in
+                            image
+                                .centerCropped()
+                                .aspectRatio(contentMode: .fill)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .shadow(radius: 5)
+                    }
+                }
+            }
+            .navigationBarTitle("Feed", displayMode: .large)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding(.horizontal, 20)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Image {
+    func centerCropped() -> some View {
+        GeometryReader { geo in
+            self
+            .resizable()
+            .scaledToFill()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .clipped()
+        }
+    }
 }
---
+++ UnsplashApp/UnsplashModel.swift
@@ -0,0 +1,119 @@
+//
+//  UnsplashModel.swift
+//  UnsplashApp
+//
+//  Created by Adrien CHENU on 1/23/24.
+//
+
+import Foundation
+
+// MARK: - UnsplashModel
+struct UnsplashModel: Codable {
+    let id, slug: String
+    let urls: Urls
+    let user: User
+}
+
+// MARK: - Urls
+struct Urls: Codable {
+    let raw, full, regular, small: String
+    let thumb, smallS3: String
+
+    enum CodingKeys: String, CodingKey {
+        case raw, full, regular, small, thumb
+        case smallS3 = "small_s3"
+    }
+}
+
+// MARK: - User
+struct User: Codable {
+    let id: String
+    let updatedAt: Date
+    let username, name, firstName, lastName: String
+    let twitterUsername: JSONNull?
+    let portfolioURL: String
+    let bio, location: String
+    let links: Links
+    let profileImage: ProfileImage
+    let instagramUsername: String
+    let totalCollections, totalLikes, totalPhotos, totalPromotedPhotos: Int
+    let acceptedTos, forHire: Bool
+    let social: Social
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case updatedAt = "updated_at"
+        case username, name
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case twitterUsername = "twitter_username"
+        case portfolioURL = "portfolio_url"
+        case bio, location, links
+        case profileImage = "profile_image"
+        case instagramUsername = "instagram_username"
+        case totalCollections = "total_collections"
+        case totalLikes = "total_likes"
+        case totalPhotos = "total_photos"
+        case totalPromotedPhotos = "total_promoted_photos"
+        case acceptedTos = "accepted_tos"
+        case forHire = "for_hire"
+        case social
+    }
+}
+
+// MARK: - Links
+struct Links: Codable {
+    let linksSelf, html, photos, likes: String
+    let portfolio, following, followers: String
+
+    enum CodingKeys: String, CodingKey {
+        case linksSelf = "self"
+        case html, photos, likes, portfolio, following, followers
+    }
+}
+
+// MARK: - ProfileImage
+struct ProfileImage: Codable {
+    let small, medium, large: String
+}
+
+// MARK: - Social
+struct Social: Codable {
+    let instagramUsername: String
+    let portfolioURL: String
+    let twitterUsername, paypalEmail: JSONNull?
+
+    enum CodingKeys: String, CodingKey {
+        case instagramUsername = "instagram_username"
+        case portfolioURL = "portfolio_url"
+        case twitterUsername = "twitter_username"
+        case paypalEmail = "paypal_email"
+    }
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
